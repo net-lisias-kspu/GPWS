c5ad90e9c03ebfd4e61138e86109e7898a9a905a
Lisias <me@lisias.net>
2021-03-20 04:36:17 -0300

    Adding seamless support for ClickThroughBlock using KSPe.

--- Source/GPWS/UI/SettingGui.cs
+++ Source/GPWS/UI/SettingGui.cs
@@ -20,6 +20,9 @@ using System;
 using UnityEngine;
 using KSP_GPWS.Interfaces;
 
+using GUI = KSPe.UI.GUI;
+using GUILayout = KSPe.UI.GUILayout;
+
 namespace KSP_GPWS.UI
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
