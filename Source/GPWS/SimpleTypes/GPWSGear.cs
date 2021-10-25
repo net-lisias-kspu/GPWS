dd83a9c9eb0f8ce3098d82f14b14cfc06999a8e0
Lisias T <me@lisias.net>
2021-10-25 15:48:56 -0300

    Legal Mambo Jambo

--- Source/GPWS/SimpleTypes/GPWSGear.cs
+++ Source/GPWS/SimpleTypes/GPWSGear.cs
@@ -1,8 +1,21 @@
-﻿// GPWS mod for KSP
-// License: CC-BY-NC-SA
-// Author: bss, 2015
+﻿/*
+	This file is part of GPWS /L Unleashed
+		© 2021 Lisias T : http://lisias.net <support@lisias.net>
+		© 2015-2018 bssthu
+		© 2013-2014 Cryphonus
 
+	GPWS /L is double licensed, as follows:
 
+		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+	GPWS /L Unleashed is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with GPWS /L Unleashed. If not, see <https://www.gnu.org/licenses/>.
+
+*/
 namespace KSP_GPWS.SimpleTypes
 {
     class GPWSGear : PartModule
